eaeee11589f733fc47bd9d72f6cb2065c91c21c1
Sri Sai Srikanth Reddy Vundyala <114636008+Srikanthreddy89@users.noreply.github.com>
2024-09-03 19:12:51 -0400

    Added project files to git

---
+++ load_model_video.py
@@ -0,0 +1,60 @@
+import numpy as np
+import cv2
+import winsound
+import smtplib
+import geocoder
+
+import faceRecognition as fr
+print (fr)
+
+
+
+face_recognizer=cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read(r'D:\Artificial Intelligence\Surveillance Detection\trainingData.yml')    #Give path of where trainingData.yml is saved
+
+cap=cv2.VideoCapture(0)   #If you want to recognise face from a video then replace 0 with video path
+
+name={0:"Ananya"}    #Change names accordingly.  If you want to recognize only one person then write:- name={0:"name"} thats all. Dont write for id number 1.
+while True:
+    ret,test_img=cap.read()
+    faces_detected,gray_img=fr.faceDetection(test_img)
+    print("face Detected: ",faces_detected)
+    for (x,y,w,h) in faces_detected:
+        cv2.rectangle(test_img,(x,y),(x+w,y+h),(0,255,0),thickness=5)
+
+    
+    
+    for face in faces_detected:
+        (x,y,w,h)=face
+        roi_gray=gray_img[y:y+h,x:x+h]
+        label,confidence=face_recognizer.predict(roi_gray)
+        print ("Confidence :",confidence)
+        print("label :",label)
+        fr.draw_rect(test_img,face)
+        predicted_name=name[label]
+        if confidence < 60:
+            fr.put_text(test_img, predicted_name, x, y)
+
+            winsound.Beep(1200, 100)
+
+            sender_email = "lastshadow0099@outlook.com"
+            rec_email = "ananya.kreddy@outlook.com"
+            password = "TimetoMail@2"
+            g = geocoder.ip('me')
+            location  =' '.join([str(item) for item in g])
+            SUBJECT = "Surveillance Detector"
+            TEXT = predicted_name + " detected in " + location
+            message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+            server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+            server.starttls()
+            server.login(sender_email, password)
+            server.sendmail(sender_email, rec_email, message)
+
+    resized_img = cv2.resize(test_img, (1000, 700))
+
+    cv2.imshow("face detection ", resized_img)
+    if cv2.waitKey(10) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows
---
+++ train_model.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+import os
+
+import faceRecognition as fr
+print (fr)
+
+
+#Training will begin from here
+
+faces,faceID=fr.labels_for_training_data(r'D:\Artificial Intelligence\Surveillance Detection\myimages') #Give path to the train-images folder which has both labeled folder as 0 and 1
+face_recognizer=fr.train_classifier(faces,faceID)
+face_recognizer.save(r'D:\Artificial Intelligence\Surveillance Detection\trainingData.yml') #It will save the trained model. Just give path to where you want to save
+
+name={0:"Ananya", 1:"Mithra"}    #Change names accordingly. If you want to recognize only one person then write:- name={0:"name"} thats all. Dont write for id number 1.1
+
+
+# for face in faces_detected:
+#     (x,y,w,h)=face
+#     roi_gray=gray_img[y:y+h,x:x+h]
+#     label,confidence=face_recognizer.predict(roi_gray)
+#     print ("Confidence :",confidence)
+#     print("label :",label)
+#     fr.draw_rect(test_img,face)
+#     predicted_name=name[label]
+#     fr.put_text(test_img,predicted_name,x,y)
+
+# resized_img=cv2.resize(test_img,(1000,700))
+
+# cv2.imshow("face detection ", resized_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
+
